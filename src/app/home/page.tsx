0709e528f7923ad8b7bb6948099682d9a3d8028e
tranduongthieu <thieu098dm3@gmail.com>
2024-12-21 12:08:41 +0700

    chores: test deployment

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './page.scss';
 const HomePage = () => {
-  return <div className="text-[1.6rem] text-red-500">HomePage</div>;
+  return <div className="text-[1.6rem] text-red-500">Home</div>;
 };
 
 export default HomePage;
