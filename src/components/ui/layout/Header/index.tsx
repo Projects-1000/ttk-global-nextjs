dcda1cfa5e1f7b07780025827daa5e3936c9c22d
lamnt1 <lamnt1@intern.firegroup.io>
2024-12-31 11:12:29 +0700

    style: added scroll event to header on mobile

--- src/components/ui/layout/Header/index.tsx
+++ src/components/ui/layout/Header/index.tsx
@@ -9,30 +9,39 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { MobileMenu } from '../SelectMenu/MobileMenu';
 
+type direction = 'up' | 'down' | null;
 export const Header = () => {
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
-  const [isTop, setIsTop] = useState<boolean>(true);
+  const [scrollDirection, setScrollDirection] = useState<direction>(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? 'down' : 'up';
+      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener('scroll', updateScrollDirection);
+    return () => {
+      window.removeEventListener('scroll', updateScrollDirection);
+    };
+  }, [scrollDirection]);
+
   const handleOpenMenu = () => {
     setIsOpenMenu(!isOpenMenu);
   };
-  useEffect(() => {
-    const onScroll = () => {
-      setIsTop(window.scrollY === 0);
-    };
 
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
-  console.log(isTop);
   return (
     <header
-      className={`fixed top-0 z-10 flex w-full justify-center container ${
-        isTop ? 'bg-transparent' : 'bg-white/30 shadow-md'
-      } backdrop-blur-md transition-colors duration-300`}
+      className={`container fixed top-0 z-10 flex w-full justify-center smooth-transition ${
+        scrollDirection === 'down' ? 'max-tablet:-translate-y-full' : 'max-tablet:translate-y-0'
+      } backdrop-blur-md`}
     >
-      <div className={`h-mobile_header laptop:h-desktop_header w-full desktop:max-w-[1440px] ${!isOpenMenu ? '' : ''}`}>
+      <div className={`h-mobile_header w-full laptop:h-desktop_header desktop:max-w-[1440px] ${!isOpenMenu ? '' : ''}`}>
         <div className="flex h-full items-center justify-between">
           <div className="flex h-full items-center gap-l">
             <div className="relative h-full w-[136px] tablet:w-[152px]">
